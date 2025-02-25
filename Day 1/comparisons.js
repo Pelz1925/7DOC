8a2abda06dd42d8a4544e1ba362be0650ff6e088
Pelz1925 <jmllg-98@outlook.com>
2025-02-25 10:11:30 -0500

    Day 1 Challenge commit

---
+++ Day 1/comparisons.js
@@ -0,0 +1,38 @@
+// Variables
+let numero_uno = 1
+let string_uno ='1'
+
+let numero_diez = 10
+let string_diez = '10'
+
+let numero_treinta = 30
+let string_treinta = '30'
+
+// Comparaciones
+if (numero_uno == string_uno) {
+    console.log('Las variables tienen el mismo valor pero no el mismo tipo');
+} else {
+    console.log('Las variables no tienen el mismo valor')
+};
+
+console.log('El valor de la variable numero_uno es: ' + numero_uno.valueOf() + ', pero el tipo es: ' + typeof(numero_uno));
+console.log('El valor de la variable string_uno es: ' + string_uno.valueOf() + ', pero el tipo es: ' + typeof(string_uno));
+
+
+if (numero_diez == string_diez) {
+    console.log('Las variables tienen el mismo valor pero no el mismo tipo');
+} else {
+    console.log('Las variables no tienen el mismo valor')
+};
+
+console.log('El valor de la variable numero_uno es: ' + numero_diez.valueOf() + ', pero el tipo es: ' + typeof(numero_diez));
+console.log('El valor de la variable string_uno es: ' + string_diez.valueOf() + ', pero el tipo es: ' + typeof(string_diez));
+
+if (numero_treinta == string_treinta) {
+    console.log('Las variables tienen el mismo valor pero no el mismo tipo');
+} else {
+    console.log('Las variables no tienen el mismo valor')
+};
+
+console.log('El valor de la variable numero_uno es: ' + numero_treinta.valueOf() + ', pero el tipo es: ' + typeof(numero_treinta));
+console.log('El valor de la variable string_uno es: ' + string_treinta.valueOf() + ', pero el tipo es: ' + typeof(string_treinta));
