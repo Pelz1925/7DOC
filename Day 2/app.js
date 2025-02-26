1ceb646ff250bd0f3473163d5d7a64dc6cd3fd35
Pelz1925 <jmllg-98@outlook.com>
2025-02-26 17:22:47 -0500

    Dia 2 del reto

---
+++ Day 2/app.js
@@ -0,0 +1,34 @@
+// Definiendo funcion
+
+function savename() {
+    let name = document.getElementById('name').value;
+    let lastname = document.getElementById('lastname').value;
+    let age = document.getElementById('age').value;
+    let language = document.getElementById('language').value;
+
+    let clearname = document.getElementById('name').value="";
+    let clearlastname = document.getElementById('lastname').value="";
+    let clearage = document.getElementById('age').value="";
+    let clearlanguage = document.getElementById('language').value="";
+
+    let message = ('Hola '+name+' '+lastname+', tienes '+age+' años y estas aprendiendo '+language);
+
+    let create_parragraph = document.createElement('p');
+    let insert_parragraph = document.getElementById('form');
+
+    create_parragraph.innerHTML = message;
+
+    insert_parragraph.appendChild(create_parragraph);
+
+    document.getElementById('name').disabled=true;
+    document.getElementById('lastname').disabled=true;
+    document.getElementById('age').disabled=true;
+    document.getElementById('language').disabled=true;
+}
+
+function reset() {
+    document.getElementById('name').disabled=false;
+    document.getElementById('lastname').disabled=false;
+    document.getElementById('age').disabled=false;
+    document.getElementById('language').disabled=false;
+}
